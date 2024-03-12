9b3f5e8b9248acdfcc78535c57f12860ac2a36c1
slava <jaws_x@mail.ru>
2024-03-12 23:14:22 +0500

    Finish_task_4

---
+++ Final control work on the basic block/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] initialArray = { "Jaw", "444", "2222", "888" };
+
+        string[] newArray = FilterArray(initialArray);
+
+        Console.WriteLine("Новый массив:");
+        foreach (string item in newArray)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    static string[] FilterArray(string[] initialArray)
+    {
+        int count = 0;
+        foreach (string item in initialArray)
+        {
+            if (item.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] newArray = new string[count];
+        int index = 0;
+        foreach (string item in initialArray)
+        {
+            if (item.Length <= 3)
+            {
+                newArray[index] = item;
+                index++;
+            }
+        }
+
+        return newArray;
+    }
+}
